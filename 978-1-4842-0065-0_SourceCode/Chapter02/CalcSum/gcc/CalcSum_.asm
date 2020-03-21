ed10193568c1371c1945fd352b25c65366fe29ee
Alejandro Gallo <aamsgallo@gmail.com>
2020-03-21 21:32:49 +0100

    Add another calc-sum

--- 978-1-4842-0065-0_SourceCode/Chapter02/CalcSum/gcc/CalcSum_.asm
+++ 978-1-4842-0065-0_SourceCode/Chapter02/CalcSum/gcc/CalcSum_.asm
@@ -1,17 +1,30 @@
 .intel_syntax noprefix
 
 .text
+
 .global CalcSum_
 CalcSum_:
   push ebp
   mov ebp,esp
 
-  mov eax,[ebp+8]  ; eax = 'a'
-  mov ecx,[ebp+12] ; ecx = 'b'
-  mov edx,[ebp+16] ; edx = 'c'
+  mov eax,[ebp+8]
+  mov ecx,[ebp+12]
+  mov edx,[ebp+16]
 
   add eax,ecx      ; eax = 'a' + 'b'
   add eax,edx      ; eax = 'a' + 'b' + 'c'
+
+  pop ebp
+  ret
+
+.global CalcSumAlternative_
+CalcSumAlternative_:
+  push ebp
+  mov ebp,esp
+
+  mov eax,[ebp+8]
+  add eax, [ebp + 12]
+  add eax, [ebp + 16]
 
   pop ebp
   ret
--- 978-1-4842-0065-0_SourceCode/Chapter02/CalcSum/gcc/main.cpp
+++ 978-1-4842-0065-0_SourceCode/Chapter02/CalcSum/gcc/main.cpp
@@ -1,15 +1,34 @@
 #include <stdio.h>
 
 extern "C" int CalcSum_(int a, int b, int c);
+extern "C" int CalcSumAlternative_(int a, int b, int c);
 
 int main(int argc, char* argv[])
 {
-    int a = 17, b = 11, c = 14;
+
+  {
+    int a = 5, b = 20, c = 213;
     int sum = CalcSum_(a, b, c);
 
     printf("a:   %d\n", a);
     printf("b:   %d\n", b);
     printf("c:   %d\n", c);
     printf("sum: %d\n", sum);
+
+  }
+
+  {
+    int a = 5, b = 20, c = 213;
+    int sum = CalcSumAlternative_(a, b, c);
+
+    printf("=========\n");
+    printf("a:   %d\n", a);
+    printf("b:   %d\n", b);
+    printf("c:   %d\n", c);
+    printf("sum: %d\n", sum);
+
+  }
+
+
     return 0;
 }
